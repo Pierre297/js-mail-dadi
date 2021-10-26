db8934a09722284c686b5fd93421e10c767b90e2
Pier <pierfrancesco297@gmail.com>
2021-10-26 14:12:43 +0200

    mail whitelist

---
+++ 01-Mail/JS/script.js
@@ -0,0 +1,22 @@
+// Chiedi all’utente la sua email,
+const userMail = prompt("Qual è la tua Email?");
+
+// whitelist
+const whiteList = ["pincopallo@gmail.com", "tpatsyg@mobii.site", "gatmanry@caraparcal.com", "datanu.gh@cuenmex.com", "dyoussaf.mokafih4@hearkn.com", "ztomino.belloukl@hacktoy.com"]
+
+// controlla che sia nella lista
+for( let i = 1; i = whiteList; i++){
+    if (i = userMail){
+
+        console.log("l'utente è parte della lista.")
+
+    }else {
+
+        console.log("l'utente non è parte della lista.")
+
+    }
+}
+
+ 
+//  stampa un messaggio appropriato sull’esito del controllo.
+
