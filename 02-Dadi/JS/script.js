156c3f9143064b8af1496c53557fa0d875681d28
Pier <pierfrancesco297@gmail.com>
2021-10-26 15:10:55 +0200

    risultato dadi

--- 02-Dadi/JS/script.js
+++ 02-Dadi/JS/script.js
@@ -1,2 +1,20 @@
 // Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
 // Stabilire il vincitore, in base a chi fa il punteggio più alto.
+const rollDice = document.getElementById("btn")
+const firstElement = document.getElementById("resoult-player")
+const secondElement = document.getElementById("resoult-pc")
+const winner = document.getElementById("")
+
+
+rollDice.addEventListener('click',
+
+function(){
+    var dice1 = Math.floor(Math.random() * 6) + 1;
+    var dice2 = Math.floor(Math.random() * 6) + 1;
+
+    firstElement.innerHTML = dice1;
+    secondElement.innerHTML = dice2;
+    
+}
+
+);
