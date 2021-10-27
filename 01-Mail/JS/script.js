8d65b97d6f221f8493c3a6412714733e39ae9b37
Pier <pierfrancesco297@gmail.com>
2021-10-27 13:46:45 +0200

    mail aggiornato

--- 01-Mail/JS/script.js
+++ 01-Mail/JS/script.js
@@ -3,20 +3,22 @@ const userMail = prompt("Qual è la tua Email?");
 const guessList = document.getElementById("guess-list");
 
 // whitelist
-const whiteList = ["pincopallo@gmail.com", "tpatsyg@mobii.site", "gatmanry@caraparcal.com", "datanu.gh@cuenmex.com", "dyoussaf.mokafih4@hearkn.com", "ztomino.belloukl@hacktoy.com"]
+const whiteList = ["pincopallo@gmail.com", "tpatsyg@mobii.site", "gatmanry@caraparcal.com", "datanu.gh@cuenmex.com", "dyoussaf.mokafih4@hearkn.com", "ztomino.belloukl@hacktoy.com"];
+
+let trovata = false;
 
 // controlla che sia nella lista
 for( let i = 0; i <= whiteList.length; i++){
     if (whiteList[i] === userMail){
-
-        guessList.innerHTML = `${userMail} è nella lista degli invitati.`;
-
-    }else {
-
-        guessList.innerHTML = `${userMail} non è nella lista degli invitati.`;
-
-
+        trovata = true;
     }
+
+};
+
+if(trovata == true ){
+    guessList.innerHTML = "Benvenuto! La tua mail è presente.";
+}else {
+    guessList.innerHTML = "Spiacenti, la tua mail non è presente nella lista.";
 };
 
  
